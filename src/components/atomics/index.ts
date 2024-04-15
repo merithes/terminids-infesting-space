87312cf9389c1456ba77e2be6df74529dfa34683
merithes <v.boivin@protonmail.com>
2024-04-15 13:05:20 +0200

    DescriptionCard missing from atomics index

--- src/components/atomics/index.ts
+++ src/components/atomics/index.ts
@@ -3,5 +3,6 @@ export { default as Separator } from './atoms/SeparatorComponent.vue'
 export { default as Skeleton } from './atoms/SkeletonComponent.vue'
 export { default as BorderDecorator } from './atoms/BorderDecorator.vue'
 export { default as TabItem } from './atoms/TabItem.vue'
+export { default as DescriptionCard } from './atoms/DescriptionCard.vue'
 
 export { default as TabList } from './molecules/TabList.vue'
